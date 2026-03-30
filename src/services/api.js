4947b40eaf28c1d1cefd540320416c8c3b3d7714
andru0603 <andresfarboleda21@gmail.com>
2026-03-29 19:12:19 -0500

    URL de producción actualizada para despliegue

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "https://peliculas-api1.onrender.com"
 });
 
 // ==================== GÉNERO ====================
